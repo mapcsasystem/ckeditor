817260b53af905b06e71d79c99380557fdb83701
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2024-01-30 07:43:14 -0600

    chek editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CKEditorModule } from 'ng2-ckeditor';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CKEditorModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ckeditor';
+  ckeditorContent: string = '<p>Some html</p>';
+  editorConfig: any = {
+    language: 'es', // Set the language to Spanish
+    // Other CKEditor configuration options
+  };
+  onChange(event: any) {
+    console.log('1', this.ckeditorContent);
+  }
+  onEditorChange(event: any) {
+    console.log('2', this.ckeditorContent);
+  }
+  onReady(event: any) {
+    console.log('3', this.ckeditorContent);
+  }
+  onFocus(event: any) {
+    console.log('4', this.ckeditorContent);
+  }
+  onBlur(event: any) {
+    console.log(this.ckeditorContent);
+  }
+  onContentDom(event: any) {
+    console.log(this.ckeditorContent);
+  }
+  onFileUploadRequest(event: any) {
+    console.log(this.ckeditorContent);
+  }
+  onFileUploadResponse(event: any) {
+    console.log(this.ckeditorContent);
+  }
+  onPaste(event: any) {
+    console.log(this.ckeditorContent);
+  }
+  onDrop(event: any) {
+    console.log(this.ckeditorContent);
+  }
 }
